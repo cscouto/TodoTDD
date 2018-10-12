4883c6c7f8fca0cc78a390977a9993101d354ca2
COUTO, TIAGO [AG-Contractor/1000] <tiago.couto@monsanto.com>
2018-10-12 13:25:53 -0500

    item manager tests

--- ToDo/Model/ItemManager.swift
+++ ToDo/Model/ItemManager.swift
@@ -9,6 +9,25 @@
 import Foundation
 
 class ItemManager {
-    let toDoCount = 0
-    let doneCount = 0
+    var toDoCount: Int { return toDoItems.count }
+    var doneCount: Int { return doneItems.count }
+    private var toDoItems: [ToDoItem] = []
+    private var doneItems: [ToDoItem] = []
+    
+    func add(_ item: ToDoItem) {
+        toDoItems.append(item)
+    }
+    
+    func item(at index: Int) -> ToDoItem {
+        return toDoItems[index]
+    }
+    
+    func checkItem(at index: Int) {
+        let item = toDoItems.remove(at: index)
+        doneItems.append(item)
+    }
+    
+    func doneItem(at index: Int) -> ToDoItem {
+        return doneItems[index]
+    }
 }
--- ToDoTests/Model/ItemManagerTests.swift
+++ ToDoTests/Model/ItemManagerTests.swift
@@ -29,4 +29,44 @@ class ItemManagerTests: XCTestCase {
     func test_DoneCount_Initially_IsZero() {
         XCTAssertEqual(sut.doneCount, 0)
     }
+    
+    func test_AddItem_IncreasesToDoCountToOne() {
+        sut.add(ToDoItem(title: ""))
+        XCTAssertEqual(sut.toDoCount, 1)
+    }
+    
+    func test_ItemAt_ReturnsAddedItem() {
+        let item = ToDoItem(title: "Foo")
+        sut.add(item)
+        let returnedItem = sut.item(at: 0)
+        XCTAssertEqual(returnedItem.title, item.title)
+    }
+    
+    func test_CheckItemAt_ChangeCounts() {
+        sut.add(ToDoItem(title: ""))
+        sut.checkItem(at: 0)
+        XCTAssertEqual(sut.toDoCount, 0)
+        XCTAssertEqual(sut.doneCount, 1)
+    }
+    
+    func test_checkItemAt_RemoveFromToDoItems() {
+        let first = ToDoItem(title: "First")
+        let second = ToDoItem(title: "Second")
+        sut.add(first)
+        sut.add(second)
+        
+        sut.checkItem(at: 0)
+        
+        XCTAssertEqual(sut.item(at: 0).title, "Second")
+    }
+    
+    func test_DoneItemAt_ReturnsCheckedItem() {
+        let item = ToDoItem(title: "Foo")
+        sut.add(item)
+        
+        sut.checkItem(at: 0)
+        let returnedItem = sut.doneItem(at: 0)
+        
+        XCTAssertEqual(returnedItem.title, item.title)
+    }
 }
