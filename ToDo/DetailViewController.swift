2069876ae537adebd154d70b8decbe1d5f46420b
Tiago Do Couto <cs.couto@outlook.com>
2019-01-02 12:10:06 -0600

    implemented detailviewcontroller tests

--- ToDo/DetailViewController.swift
+++ ToDo/DetailViewController.swift
@@ -12,11 +12,44 @@ import MapKit
 class DetailViewController: UIViewController {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var mapView: MKMapView!
+    @IBOutlet weak var labelDate: UILabel!
+    @IBOutlet weak var labelLocation: UILabel!
+    @IBOutlet weak var labelDescription: UILabel!
+    
+    var itemInfo: (ItemManager, Int)?
+    let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter
+    }()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let itemInfo = itemInfo else { return }
+        let item = itemInfo.0.item(at: itemInfo.1)
+        
+        titleLabel.text = item.title
+        labelLocation.text = item.location?.name
+        labelDescription.text = item.itemDescription
+        
+        if let timestamp = item.timestamp {
+            let date = Date(timeIntervalSince1970: timestamp)
+            labelDate.text = dateFormatter.string(from: date)
+        }
+        
+        if let coordinate = item.location?.coordinate {
+            let region = MKCoordinateRegion(center: coordinate,
+                                            latitudinalMeters: 100,
+                                            longitudinalMeters: 100)
+            mapView.region = region
+        }
+    }
+    
+    func checkItem() {
+        if let itemInfo = itemInfo {
+            itemInfo.0.checkItem(at: itemInfo.1)
+        }
     }
 
 }
--- ToDoTests/DetailViewControllerTests.swift
+++ ToDoTests/DetailViewControllerTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import CoreLocation
 @testable import ToDo
 
 class DetailViewControllerTests: XCTestCase {
@@ -27,8 +28,45 @@ class DetailViewControllerTests: XCTestCase {
         XCTAssertTrue(titleLabelIsSubView)
     }
     
+    func test_HasDateLabel() {
+        let labelDateIsSubView = sut.labelDate.isDescendant(of: sut.view)
+        XCTAssertTrue(labelDateIsSubView)
+    }
+    
     func test_HasMapView() {
         let mapViewIsSubView = sut.mapView?.isDescendant(of: sut.view) ?? false
         XCTAssertTrue(mapViewIsSubView)
+    }
+    
+    func test_SettingItemInfo_SetsTextsToLabels() {
+        let coordinate = CLLocationCoordinate2DMake(51.2277, 6.7735)
+        let location = Location(name: "Foo", coordinate: coordinate)
+        let item = ToDoItem(title: "Bar",
+                            itemDescription: "Baz",
+                            timestamp: 1456150025,
+                            location: location)
+        
+        let itemManager = ItemManager()
+        itemManager.add(item)
+        
+        sut.itemInfo = (itemManager, 0)
+        
+        sut.beginAppearanceTransition(true, animated: true)
+        sut.endAppearanceTransition()
+        XCTAssertEqual(sut.titleLabel.text, item.title)
+        XCTAssertEqual(sut.labelDate.text, "02/22/2016")
+        XCTAssertEqual(sut.labelLocation.text, "Foo")
+        XCTAssertEqual(sut.labelDescription.text, "Baz")
+    }
+    
+    func test_CheckItem_ChecksItemInItemManager() {
+        let itemManager = ItemManager()
+        itemManager.add(ToDoItem(title: "Foo"))
+        
+        sut.itemInfo = (itemManager, 0)
+        sut.checkItem()
+        
+        XCTAssertEqual(itemManager.toDoCount, 0)
+        XCTAssertEqual(itemManager.doneCount, 1)
     }
 }
